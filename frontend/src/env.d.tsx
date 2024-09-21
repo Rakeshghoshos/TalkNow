5007d9466b504c92cc92aa74e88de5870199d853
Rakesh Ghosh <142241392+Rakeshghoshos@users.noreply.github.com>
2024-09-21 09:42:34 -0700

    Update env.d.tsx

--- frontend/src/env.d.tsx
+++ frontend/src/env.d.tsx
@@ -1,5 +1,9 @@
+// const environment = {
+//     socket_url : "http://localhost:7890"
+// }
+
 const environment = {
-    socket_url : "http://localhost:7890"
+    socket_url : "https://talk-now-wheat.vercel.app"
 }
 
 export default environment;
