7d72f1206d49b03f81075d61d89b12a00a065dc6
Rakesh Ghosh <142241392+Rakeshghoshos@users.noreply.github.com>
2024-09-21 22:57:45 -0700

    Update env.d.tsx

--- frontend/src/env.d.tsx
+++ frontend/src/env.d.tsx
@@ -2,8 +2,12 @@
 //     socket_url : "http://localhost:7890"
 // }
 
+// const environment = {
+//     socket_url : "https://talk-now-virid.vercel.app"
+// }
+
 const environment = {
-    socket_url : "https://talk-now-virid.vercel.app"
+    socket_url : "https://talknow-7a3a.onrender.com"
 }
 
 export default environment;
